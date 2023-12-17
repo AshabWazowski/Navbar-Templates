6a71b27ee63744146d71589f6e73106ef9b3041a
Ashab Abidi <ashab340.abidi@gmail.com>
2023-12-17 15:16:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// import Navbar from './components/Social Media Navbar/Navbar';
+import Navbar from './components/eCommerse Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+
+      {/*Social Media
+       <Navbar/> 
+       */}
+       <Navbar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Assets/BoxModel.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const MainBox = styled(Box)({
+    display: 'flex',
+    justifyContent:'center',
+    alignItems:'center'
+});
+
+export const FlexBox = styled(Box)({
+    display:'flex',
+    justifyContent:'space-between',
+    alignItems:'center'
+});
+
+
+
